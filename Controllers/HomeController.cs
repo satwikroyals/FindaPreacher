59f09b147d75d989776e33a9e98cf198d1a7333e
kolaventi satwik <satwikroyals101097@gmail.com>
2023-11-04 20:00:12 +0530

    FindaPreacher

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,98 @@
+﻿
+using FindaPreacher_1_.edmx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FindaPreacher.Controllers
+{
+    public class HomeController : Controller
+    {
+         FindaPreacherEntities2 db =new FindaPreacherEntities2();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult Event()
+        {
+            List<Event> e = db.Events.ToList();
+            return View(e);
+        }
+        public ActionResult AddEvent()
+        {
+            List<Event> eve = db.Events.ToList();
+
+            //List<MinistryType> m = db.MinistryTypes.ToList();
+            //ViewBag.MinistryTypeList = new SelectList(m, "MinistryTypeID", "MinistryType1");
+
+            //List<EventHelpService> Help= db.EventHelpServices.ToList();
+            //ViewBag.MinistryHelpList = new SelectList(Help, "HelpServiceTypeID", "Message");
+            return View(eve);
+        }
+      [HttpPost]
+      public ActionResult AddEvent(Event eve)
+        {
+            db.Events.Add(eve);
+            db.SaveChanges();
+
+            ViewBag.Message = "Successfully Submitted";
+            return RedirectToAction("Event");
+        }
+        public ActionResult Delete(int eid)
+        {
+            
+            
+                Event eve = db.Events.ToList().Where(x => x.EventID == eid).FirstOrDefault();
+
+            return View(eve);
+            
+        }
+        
+
+        
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            Event eve = db.Events.Find(id);
+            db.Events.Remove(eve);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Update(int eveid)
+        {
+            Event eve = db.Events.ToList().Where(x => x.EventID == eveid).FirstOrDefault();
+
+            return View(eve);
+
+
+        }
+
+        [HttpPost]
+        public ActionResult Update(Event e)
+        {
+
+            db.Events.ToList();
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Controllers/UsersController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FindaPreacher_1_.edmx;
+
+namespace FindaPreacher.Controllers
+{
+    public class UsersController : Controller
+    {
+        private FindaPreacherEntities2 db = new FindaPreacherEntities2();
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View(db.Users.ToList());
+        }
+
+        // GET: Users/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+         
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UserID,FirstName,LastName,Email,Mobile,Pin,CountryID,Address,Longitude,Latitude,City,PostCode,ProfilePic,UserTypeID,Status,CreatedDate,ModifiedDate,DenominationTypeId,Qualification,Details,OrganisationName")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Edit/5
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserID,FirstName,LastName,Email,Mobile,Pin,CountryID,Address,Longitude,Latitude,City,PostCode,ProfilePic,UserTypeID,Status,CreatedDate,ModifiedDate,DenominationTypeId,Qualification,Details,OrganisationName")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            User user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
